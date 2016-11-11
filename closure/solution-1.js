d70ad1bfa0fc0b72e78719455b345488433603d2
Sai Gudigundla <saip106@msn.com>
2016-11-11 15:28:32 -0500

    fixed formatting

--- closure/solution-1.js
+++ closure/solution-1.js
@@ -1,14 +1,11 @@
 function printWithDelay(people){
     for (var index = 0; index < people.length; index++) {
-
         (function () {
             var  i = index;
             setTimeout(function() {
                 console.log(people[i]);
             }, 1000);
         })();
-
-        
     }
 }
 
--- closure/solution-2.js
+++ closure/solution-2.js
@@ -1,13 +1,10 @@
 function printWithDelay(people){
     for (var index = 0; index < people.length; index++) {
-
         (function (index) {
             setTimeout(function() {
                 console.log(people[index]);
             }, 1000);
         })(index);
-
-        
     }
 }
 
